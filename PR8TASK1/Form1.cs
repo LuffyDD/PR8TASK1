35d0b25f72024d0a0784d8fff169cb92303d43b7
kosovskiy989 <kosovskiy989@gmail.com>
2021-04-12 16:22:21 +0300

    Добавьте файлы проекта.

---
+++ PR8TASK1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR8TASK1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x1 = double.Parse(textBox1.Text);
+            double x2 = double.Parse(textBox2.Text);
+            double y = Math.Sqrt(56 * x1 + ((x1 + x2 + Math.Sin(x1 * x2)) / 5 - Math.Cos(Math.Pow(x2, 2))));
+            textBox3.Text = Convert.ToString(y);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            textBox2.Clear();
+            textBox1.Clear();
+        }
+    }
+}
